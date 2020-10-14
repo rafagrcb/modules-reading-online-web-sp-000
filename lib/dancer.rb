b8fec4c761ed10b438486f7b134e0acdf25a9cde
Rafael Garcia <rafagrcb@gmail.com>
2020-10-14 19:39:28 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dancer
+  include Dance
   extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
